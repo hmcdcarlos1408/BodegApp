6b5a0aa1c201d93c8bdf8b206bafbdb575f2c043
hmcdcarlos1408 <117197233+hmcdcarlos1408@users.noreply.github.com>
2023-03-06 22:30:16 -0600

    navigation

---
+++ BodegApp/Screems/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import  {View,Text} from "react-native"
+
+const HomeScreen=()=>{
+    return(<View>
+        <Text>simon aqui esta el home</Text>
+    </View>)
+}
+export default HomeScreen;
