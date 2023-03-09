3e348848cac11ced7e051e205514072cfdb13a9c
RedDark998 <orockero5@gmail.com>
2023-03-09 12:12:40 -0600

    Cambio de pantalla Home para inicio de sesion

--- BodegApp/Screens/HomeScreen.js
+++ BodegApp/Screens/HomeScreen.js
@@ -1,10 +1,71 @@
-import React from "react";
-import  {View,Text} from "react-native"
+import * as React from "react";
+import { Box, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider } from "native-base";
+import SignupScreen from './SignupScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-const HomeScreen=()=>{
-    return(<View>
-        <Text>simon aqui esta el home</Text>
-    </View>)
-}
 
-export default HomeScreen;
+const Example = () => {
+  return <Center w="100%">
+      <Box safeArea p="2" py="8" w="90%" maxW="290" style={{textAlign: 'center'}}>
+        <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
+        color: "warmGray.50"
+      }}>
+          !Bienvenidos¡
+        </Heading>
+        <Heading mt="1" _dark={{
+        color: "warmGray.200"
+      }} color="coolGray.600" fontWeight="medium" size="xs" space={1} >
+          Inicia Sesion para continar
+        </Heading>
+
+        <VStack space={2} mt="5">
+          <FormControl>
+            <FormControl.Label>Correo Electronico</FormControl.Label>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Contraseña</FormControl.Label>
+            <Input type="password" />
+            <Link _text={{
+            fontSize: "xs",
+            fontWeight: "500",
+            color: "indigo.500"
+          }} alignSelf="flex-end" mt="1">
+              ¿Olvidaste tu contraseña?
+            </Link>
+          </FormControl>
+          <Button mt="2" colorScheme="indigo">
+            Iniciar Sesion
+          </Button>
+          <HStack mt="6" justifyContent="center">
+            <Text fontSize="sm" color="coolGray.600" _dark={{
+            color: "warmGray.200"
+          }}>
+              Soy un usuario nuevo.{" "}
+            </Text>
+            <Link _text={{
+            color: "indigo.500",
+            fontWeight: "medium",
+            fontSize: "sm"
+          }} href="#">
+              Registrarme 
+            </Link>
+          </HStack>
+        </VStack>
+      </Box>
+    </Center>;
+    
+};
+
+    export default () => {
+        return (
+          <NativeBaseProvider>
+            <Center flex={1} px="3">
+                <Example />
+            </Center>
+          </NativeBaseProvider>
+        );
+    };
+
+    
--- BodegApp/Screens/SignupScreen.js
+++ BodegApp/Screens/SignupScreen.js
@@ -7,28 +7,28 @@ const Example = () => {
         <Heading size="lg" color="coolGray.800" _dark={{
         color: "warmGray.50"
       }} fontWeight="semibold">
-          Welcome
+          !Bienvenidos¡
         </Heading>
         <Heading mt="1" color="coolGray.600" _dark={{
         color: "warmGray.200"
       }} fontWeight="medium" size="xs">
-          Sign up to continue!
+          ¡Regístrate para continuar!
         </Heading>
         <VStack space={3} mt="5">
           <FormControl>
-            <FormControl.Label>Email</FormControl.Label>
+            <FormControl.Label>Correo</FormControl.Label>
             <Input />
           </FormControl>
           <FormControl>
-            <FormControl.Label>Password</FormControl.Label>
+            <FormControl.Label>Contraseña</FormControl.Label>
             <Input type="password" />
           </FormControl>
           <FormControl>
-            <FormControl.Label>Confirm Password</FormControl.Label>
+            <FormControl.Label>Confirmar contraseña</FormControl.Label>
             <Input type="password" />
           </FormControl>
           <Button mt="2" colorScheme="indigo">
-            Sign up
+            Registrarse
           </Button>
         </VStack>
       </Box>
