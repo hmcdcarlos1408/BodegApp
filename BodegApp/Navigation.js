1e547a74820647e9805c5bf5ba1b8b22fae87d6f
RedDark998 <orockero5@gmail.com>
2023-03-13 12:12:41 -0600

    Navigation Pantalla Registro y acomodo

--- BodegApp/Navigation.js
+++ BodegApp/Navigation.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from "./Screens/HomeScreen"
 import Login from "./Screens/Login";
+import SignupScreen from "./Screens/SignupScreen";
 
 const Tab= createBottomTabNavigator();
 
@@ -11,8 +12,11 @@ const Tab= createBottomTabNavigator();
 function Tabs(){
     return(
         <Tab.Navigator>
-        <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={Login} />
+        <Tab.Screen name="Iniciar Sesion" component={HomeScreen} />    
+        <Tab.Screen name="Registro" component={SignupScreen} />
+
+        
       
       </Tab.Navigator>
     
